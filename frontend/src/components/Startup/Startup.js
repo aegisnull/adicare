deadd7f868a9dd12793c91f60824fb147923100d
Luis Tellez <luis@luistellez.com>
2023-01-28 15:43:30 -0800

    style: adjust styling of Startup component

--- frontend/src/components/Startup/Startup.js
+++ frontend/src/components/Startup/Startup.js
@@ -1,15 +1,16 @@
-import InfirmaryIcon from '../../images/infirmary.gif';
 import Logo from '../../images/logo.svg';
+import SplashIcon from '../../images/splash.svg';
 import './Startup.scss';
 
 function Startup() {
   return (
     <div className='startup'>
       <img src={Logo} className='startup__logo' alt='Logo' />
-      <img src={InfirmaryIcon} alt='Infirmary' className='startup__icon' />
-      <h1 className='startup__title'>
-        Tu <span className='startup__title_bold'>agenda</span> de pacientes personal
-      </h1>
+      <img src={SplashIcon} alt='Infirmary' className='startup__icon' />
+      <h1 className='startup__title'>Toma el control</h1>
+      <p className='startup__subtitle'>
+        Tu agenda de pacientes en un solo lugar y siempre bajo control
+      </p>
       <div className='startup__buttons'>
         <button className='button'>Iniciar sesión</button>
         <button className='button button_accent'>Registrarse</button>
