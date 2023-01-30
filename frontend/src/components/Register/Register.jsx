c612ba45f89ba619bb29f2604f540cddb208841e
Luis Tellez <luis@luistellez.com>
2023-01-30 15:55:27 -0800

    style: add styling to Register Form

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -8,18 +8,27 @@ function Register() {
         <img src={RegisterIcon} alt='Register' className='register__icon' />
         <h1 className='register__title'>Crea una cuenta</h1>
         <form className='register__form'>
-          <div className='register__form__group'>
-            <label htmlFor='name'>Nombre</label>
-            <input type='text' name='name' id='name' />
-          </div>
-          <div className='register__form__group'>
-            <label htmlFor='email'>Correo electrónico</label>
-            <input type='email' name='email' id='email' />
-          </div>
-          <div className='register__form__group'>
-            <label htmlFor='password'>Contraseña</label>
-            <input type='password' name='password' id='password' />
-          </div>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            placeholder='Nombre'
+            className='register__input'
+          />
+          <input
+            type='email'
+            name='email'
+            id='email'
+            placeholder='Email'
+            className='register__input'
+          />
+          <input
+            type='password'
+            name='password'
+            id='password'
+            placeholder='Contraseña'
+            className='register__input'
+          />
           <button className='button button_accent'>Crear cuenta</button>
         </form>
       </div>
