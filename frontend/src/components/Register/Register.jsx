017433353ad6b7a8d3973f8f8834387f439b019b
Luis Tellez <luis@luistellez.com>
2023-01-30 14:57:41 -0800

    feat: add icon for Register component

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -1,9 +1,11 @@
 import './Register.scss';
+import RegisterIcon from '../../images/register.svg';
 
 function Register() {
   return (
     <section className='register'>
       <div className='register__container'>
+        <img src={RegisterIcon} alt='Register' className='register__icon' />
         <h1 className='register__title'>Crea una cuenta</h1>
         <form className='register__form'>
           <div className='register__form__group'>
