15753e2665983ed6174b8ce1ad17c2aabeabd4f7
Luis Tellez <luis@luistellez.com>
2023-01-30 16:28:26 -0800

    feat: add SignIn component

---
+++ frontend/src/components/SignIn/SignIn.jsx
@@ -0,0 +1,43 @@
+import './SignIn.scss';
+import RegisterIcon from '../../images/register.svg';
+
+function SignIn() {
+  return (
+    <section className='register'>
+      <div className='register__container'>
+        <img src={RegisterIcon} alt='Register' className='register__icon' />
+        <h1 className='register__title'>Iniciar Sesión</h1>
+        <form className='register__form'>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            placeholder='Nombre'
+            className='register__input'
+          />
+          <input
+            type='email'
+            name='email'
+            id='email'
+            placeholder='Email'
+            className='register__input'
+          />
+          <input
+            type='password'
+            name='password'
+            id='password'
+            placeholder='Contraseña'
+            className='register__input'
+          />
+          <button className='button button_accent'>Iniciar sesión</button>
+        </form>
+        <p className='register__notice'>
+          Al crear una cuenta o iniciar sesión, aceptas nuestros
+          <span className='register_bold'> Términos y Condiciones</span>.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default SignIn;
